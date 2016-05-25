7dec383b00cdaef5569a2affaff5a4e76be236c1
BJ Dierkes <derks@datafolklabs.com>
2016-05-25 14:30:33 -0500

    Improve/Simplify Versioning with Variables

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e -x
 
+SERVER_VERSION="12.6.0"
+SERVER_SHA1="eafc7aedd4966457cd77eb51f75da863947dde70"
+CLIENT_VERSION="12.10.24"
+CLIENT_SHA1="7d30b300f95f00036919ee8bf3b95ab73429e57e"
+
 # Temporary work dir
 tmpdir="`mktemp -d`"
 cd "$tmpdir"
@@ -11,15 +16,15 @@ apt-get update -q --yes
 apt-get install -q --yes logrotate vim-nox hardlink wget ca-certificates
 
 # Download and install Chef's packages
-wget -nv https://packages.chef.io/stable/ubuntu/14.04/chef-server-core_12.6.0-1_amd64.deb
-wget -nv https://packages.chef.io/stable/ubuntu/10.04/chef_12.10.24-1_amd64.deb
+wget -nv https://packages.chef.io/stable/ubuntu/14.04/chef-server-core_${SERVER_VERSION}-1_amd64.deb
+wget -nv https://packages.chef.io/stable/ubuntu/10.04/chef_${CLIENT_VERSION}-1_amd64.deb
 
 sha1sum -c - <<EOF
-eafc7aedd4966457cd77eb51f75da863947dde70  chef-server-core_12.6.0-1_amd64.deb
-7d30b300f95f00036919ee8bf3b95ab73429e57e  chef_12.10.24-1_amd64.deb
+${SERVER_SHA1}  chef-server-core_${SERVER_VERSION}-1_amd64.deb
+${CLIENT_SHA1}  chef_${CLIENT_VERSION}-1_amd64.deb
 EOF
 
-dpkg -i chef-server-core_12.4.1-1_amd64.deb chef_12.7.2-1_amd64.deb
+dpkg -i chef-server-core_${SERVER_VERSION}-1_amd64.deb chef_${CLIENT_VERSION}-1_amd64.deb
 
 # Extra setup
 rm -rf /etc/opscode
